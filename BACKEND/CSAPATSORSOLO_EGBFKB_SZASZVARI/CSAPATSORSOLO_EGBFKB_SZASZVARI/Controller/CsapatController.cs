98c2add9e5485ae323ba559d7279819e4cd6a7f4
SpyderHUN <szaszvari.mark@gmail.com>
2025-05-07 00:16:54 +0200

    Controller getmember és createmember

--- BACKEND/CSAPATSORSOLO_EGBFKB_SZASZVARI/CSAPATSORSOLO_EGBFKB_SZASZVARI/Controller/CsapatController.cs
+++ BACKEND/CSAPATSORSOLO_EGBFKB_SZASZVARI/CSAPATSORSOLO_EGBFKB_SZASZVARI/Controller/CsapatController.cs
@@ -1,14 +1,34 @@
 ﻿using CSAPATSORSOLO_EGBFKB_SZASZVARI.Data;
+using CSAPATSORSOLO_EGBFKB_SZASZVARI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSAPATSORSOLO_EGBFKB_SZASZVARI.Controller
 {
-    public class CsapatController
+    [ApiController]
+    [Route("[controller]")]
+    public class CsapatController : ControllerBase
     {
         ICsapatRepository repo;
         public CsapatController(ICsapatRepository repo) 
         {
             this.repo = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Member> GetMembers()
+        {
+            return this.repo.Read();
+        }
+
+        [HttpPost]
+        public ActionResult<Member> CreateMember([FromBody] Member member)
+        {
+            if (member.Age <= 8)
+            {
+                return BadRequest("Too young! Should be older than 8!");
+            }
+            this.repo.Create(member);
+            return member;
         }
     }
 }
